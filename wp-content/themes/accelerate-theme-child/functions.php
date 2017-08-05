c395fac774cf87a8e536c45b4bb4ad81bf0a2f96
Bohemian Codes <chavelitaloca@gmail.com>
2017-08-05 19:33:07 +0200

    fixing contact-us page

--- wp-content/themes/accelerate-theme-child/functions.php
+++ wp-content/themes/accelerate-theme-child/functions.php
@@ -41,3 +41,12 @@ function create_custom_post_types() {
     );
 }
 add_action( 'init', 'create_custom_post_types' );
+
+//Add body class
+function accelerate_child_body_classes( $classes ) {
+  if ( is_page( 'contact-us' ) ) {
+    $classes[] = 'contact-page';
+  }
+    return $classes;
+}
+add_filter( 'body_class','accelerate_child_body_classes' );
